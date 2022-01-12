5d1caf8f089567ea111f4647bc09a1de2678dc53
PARK JI YOUNG <rwssss52@gmail.com>
2022-01-12 15:21:08 +0900

    박지영 : while문을 사용한 배열에 짝수 넣기

---
+++ Homwwork/Homwwork/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+//while 문
+int main()
+{
+	const int arrIndex = 10;
+	int arr[arrIndex] = { 0, };
+
+	int Index = 0, i = 0;
+
+	while (true) //index != arrIndex
+	{
+		if (i % 2 == 0) //나머지 값이 0 이라면
+		{
+			arr[Index] = i;
+
+			//cout << arr[Index] << endl; 
+			++Index;
+		}
+		if (Index >= arrIndex) break;
+		i++;
+	}
+
+	return 0;
+}
+
+//for 문
+//int main()
+//{
+//
+//	const int ArrIndex = 10;
+//	int Arr[ArrIndex] = { 0, };
+//
+//	int index = 0;
+//
+//	for (int i = 0; index < ArrIndex; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			Arr[index] = i;
+//
+//			//cout << Arr[index] << endl;
+//			index++;
+//		}
+//	}
+//
+//
+//}
